6cee5d2743d9737740f4446a39438e69b4ad77c5
Geferson Pillaca <gpillacag@gmail.com>
2020-09-25 00:08:58 -0500

    [list-counters] add Dagger Hilt

--- app/src/main/java/com/gpillaca/counters/CounterApp.kt
+++ app/src/main/java/com/gpillaca/counters/CounterApp.kt
@@ -1,5 +1,7 @@
 package com.gpillaca.counters
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class CounterApp : Application()
---
+++ app/src/main/java/com/gpillaca/counters/di/AppModule.kt
@@ -0,0 +1,9 @@
+package com.gpillaca.counters.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class AppModule
