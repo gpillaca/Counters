68bd1a5ae80904660cefc2bb626538e9443a0adc
Geferson Pillaca <gpillacag@gmail.com>
2020-09-30 12:21:07 -0500

    remove code unnecessary

--- app/src/main/java/com/gpillaca/counters/CounterApp.kt
+++ app/src/main/java/com/gpillaca/counters/CounterApp.kt
@@ -4,9 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CounterApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-    }
-}
+class CounterApp : Application()
